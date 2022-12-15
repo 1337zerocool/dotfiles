668aa64f67f40ecc403ccb3e3ac8b25a7bf402a8
Colin Stein <Colin Stein>
2022-12-15 12:14:27 -0700

    Add automatic checkout of git submodules

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 tic -x -o $HOME/.terminfo $HOME/dotfiles/config/terminfo/xterm-kitty &>/dev/null
 
 # install some ZSH features: autosuggestions and syntax highlighting
-# git clone https://github.com/zsh-users/zsh-autosuggestions
-# git clone https://github.com/zdharma-continuum/fast-syntax-highlighting
+# You could check these out manually with
+#   git clone https://github.com/zsh-users/zsh-autosuggestions
+#   git clone https://github.com/zdharma-continuum/fast-syntax-highlighting
+# but they're added to this repo as a submodule
+git --git-dir "$HOME/dotfiles/.git/" submodule update --init --recursive
