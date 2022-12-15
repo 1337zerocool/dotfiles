aa4ea881d23425ac00a609e1e707c92edf9abb2d
Colin Stein <Colin Stein>
2022-12-15 11:16:41 -0700

    Update details of ZSH plugins

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ tic -x -o $HOME/.terminfo $HOME/dotfiles/config/terminfo/xterm-kitty &>/dev/null
 
 # install some ZSH features: autosuggestions and syntax highlighting
 # git clone https://github.com/zsh-users/zsh-autosuggestions
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting
-# 
+# git clone https://github.com/zdharma-continuum/fast-syntax-highlighting
