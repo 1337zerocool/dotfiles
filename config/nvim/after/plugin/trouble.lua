f9bf9640b3270e321b7b0fd1b2c65973bc814031
Colin Stein <colin.stein@shopify.com>
2023-03-09 06:56:36 -0700

    get better handling of line level warnings

--- config/nvim/after/plugin/trouble.lua
+++ config/nvim/after/plugin/trouble.lua
@@ -6,6 +6,6 @@ trouble.setup({
   mode = 'document_diagnostics',
   height = 5,
 })
-vim.keymap.set('n', '<leader>ee', '<cmd>TroubleToggle document_diagnostics<cr>', { silent = true })
-vim.keymap.set('n', '<leader>EE', '<cmd>TroubleToggle workspace_diagnostics<cr>', { silent = true })
+vim.keymap.set('n', '<leader>EE', '<cmd>TroubleToggle document_diagnostics<cr>', { silent = true })
+vim.keymap.set('n', '<leader>EEE', '<cmd>TroubleToggle workspace_diagnostics<cr>', { silent = true })
 vim.keymap.set('n', '<leader>R', '<cmd>TroubleToggle lsp_references<cr>', { silent = true })
