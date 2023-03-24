9ee1f12aad45c5afff941b5e66796142c0921dbf
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2023-03-23 18:58:43 -0600

    Some settings for editing mackrown prose

---
+++ config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+vim.opt.formatoptions = 'ant' -- a = automatic format paragraphs on insert & delete
+                              -- t = autowrap textwidth
+                              -- n = recognize numbered lists
+vim.opt.textwidth = 80
+vim.opt.smartindent = false
+vim.opt.wrapmargin = 0
+vim.opt.autoindent = false
+vim.opt.cindent = false
+vim.opt.indentexpr = ''
