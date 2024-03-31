ac29cdc42ff173346907684789ab90dcb6e8ff94
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2024-03-31 14:43:59 -0600

    Diffview is now a toggle

--- config/nvim/after/plugin/diffview.lua
+++ config/nvim/after/plugin/diffview.lua
@@ -1,5 +1,17 @@
-local ok, _ = pcall(require, 'diffview')
-if not ok then
+-- local ok, diffview = pcall(require, 'diffview')
+-- if not ok then
+--   return
+-- end
+
+local ok_lib, diffviewlib = pcall(require, 'diffview.lib')
+if not ok_lib then
   return
 end
-vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<cr>')
+
+vim.keymap.set('n', '<leader>gd', function()
+  if next(diffviewlib.views) == nil then
+    vim.cmd('DiffviewOpen')
+  else
+    vim.cmd('DiffviewClose')
+  end
+end)
