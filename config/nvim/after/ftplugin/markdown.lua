6adad46a0331cbe3ff7599df7cceb50b8e9aed13
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2024-04-10 21:18:46 -0600

    Making things nicer ro ruby

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
-vim.opt.formatoptions = 'ant' -- a = automatic format paragraphs on insert & delete
+vim.opt.formatoptions = 'nt' -- a = automatic format paragraphs on insert & delete
                               -- t = autowrap textwidth
                               -- n = recognize numbered lists
 vim.opt.textwidth = 80
