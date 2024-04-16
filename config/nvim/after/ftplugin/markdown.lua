c208c8aea3527d9835e4600df80fc217f6904292
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2024-04-15 19:49:56 -0600

    drop dumb markdown option

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,11 +1,8 @@
-vim.opt.formatoptions = 'nt' -- a = automatic format paragraphs on insert & delete
-                              -- t = autowrap textwidth
-                              -- n = recognize numbered lists
+vim.opt.formatoptions = 'nt' -- t = autowrap textwidth
+                             -- n = recognize numbered lists
 vim.opt.textwidth = 80
 vim.opt.smartindent = false
 vim.opt.wrapmargin = 0
 vim.opt.autoindent = false
 vim.opt.cindent = false
 vim.opt.indentexpr = ''
-
-vim.cmd('TSDisable highlight')
