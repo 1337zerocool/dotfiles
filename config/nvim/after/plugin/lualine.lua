69cc7dacca50cee4b24412315ab1d315c17c8ef0
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2024-04-30 15:32:30 -0600

    one more nord removal

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -5,7 +5,7 @@ end
 lualine.setup({
   options = {
     icons_enabled = true,
-    theme = 'nord',
+    theme = 'catppuccin',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
