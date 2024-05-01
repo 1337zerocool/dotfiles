15b8cae0df1cd1a1f44a35c60241ff3d9774c82d
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2024-04-30 20:17:29 -0600

    Font tidy up, color improvement
    Remove the misleading "disable_ligatures" option because it's not
    working in kitty. Having it absent will remind me to look for it later
    
    Also, dim the listchars characters in the color scheme by setting a
    custom function override in the theme

--- config/nvim/after/plugin/catppuccin.lua
+++ config/nvim/after/plugin/catppuccin.lua
@@ -34,10 +34,14 @@ color.setup({
     properties = {},
     types = {},
     operators = {},
-    -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    --  miscs = {}, -- Uncomment to turn off hard-coded styles
   },
   color_overrides = {},
-  custom_highlights = {},
+  custom_highlights = function(colors)
+    return {
+      NonText = { fg = colors.surface0 }, -- dimming listchars
+    }
+  end,
   default_integrations = true,
   integrations = {
     cmp = true,
