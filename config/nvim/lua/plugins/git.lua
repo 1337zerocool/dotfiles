2788b2326628924cf1b224c35cb64d2b210a935d
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2024-09-13 11:55:35 -0600

    get some nice git integration going on

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -2,18 +2,20 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 local gitsigns = {
   "lewis6991/gitsigns.nvim",
-  
--- gitsigns.setup({
-   -- signcolumn = false,
-   -- current_line_blame = false,
--- })
-
--- figure out how to get the gd to be on the right side with the new code
--- vim.keymap.set({ 'n', 'v' }, '<leader>gs', gitsigns.toggle_signs, { silent = true, desc = "Git show changes in sign column" })
--- vim.keymap.set({ 'n', 'v' }, '<leader>gb', function() gitsigns.blame_line({ full = true }) end, { silent = true, desc = "Git blame the current line" })
--- vim.keymap.set({ 'n', 'v' }, '<leader>gd', gitsigns.diffthis, { silent = true, desc = "Git diff this file" })
--- vim.keymap.set({ 'n', 'v' }, ']g', gitsigns.next_hunk, { silent = true, desc = "Git next hunk" })
--- vim.keymap.set({ 'n', 'v' }, '[g', gitsigns.prev_hunk, { silent = true, desc = "Git previous hunk" })
+  cmd = "Gitsigns",
+  keys = {
+    { "<leader>gs", "<cmd>Gitsigns toggle_signs<cr>", mode = { "n", "v" }, desc = "Git: toggle showing git changes in the sign column" },
+    { "<leader>gb", "<cmd>Gitsigns blame_line<cr>", mode = { "n", "v" }, desc = "Git: blame the current line" },
+    { "<leader>gB", "<cmd>Gitsigns blame<cr>", mode = { "n", "v" }, desc = "Git: blame the the buffer" },
+    { "]g", "<cmd>Gitsigns next_hunk<cr>", mode = { "n", "v" }, desc = "Git: next hunk" },
+    { "[g", "<cmd>Gitsigns prev_hunk<cr>", mode = { "n", "v" }, desc = "Git: previous hunk" },
+  },
+  config = function()
+    require('gitsigns').setup({
+      signcolumn = false,
+      current_line_blame = false,
+    })
+  end
 }
 
 -- Generates links to code on popular source-sharing websites like github and
